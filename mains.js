292645c5683c19fbf99d9bc8f8180caa28557812
ibrahimkonlan <haruna.konlan@amalitech.org>
2021-11-30 11:51:15 +0000

    first commit

---
+++ mains.js
@@ -0,0 +1,71 @@
+let userName = 'Konlan';
+userName ? console.log(`Hello, Konlan`) : console.log("Hello!");
+let userQuestion = "Can you teach me how to code?";
+console.log(`The user asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() *8);
+let eightBall = '';
+
+switch(randomNumber){
+  case 0:
+  eightBall = 'It is certain';
+  break;
+  case 1:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 2:
+  eightBall = 'It is decidedly so';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = "Don't count on it";
+  break;
+  case 5:
+  eightBall = 'My sources say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  case 7:
+  eightBall = 'Signs point to yes';
+  break;
+  
+}
+
+console.log(eightBall);let userName = 'Konlan';
+userName ? console.log(`Hello, Konlan`) : console.log("Hello!");
+let userQuestion = "Can you teach me how to code?";
+console.log(`The user asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() *8);
+let eightBall = '';
+
+switch(randomNumber){
+  case 0:
+  eightBall = 'It is certain';
+  break;
+  case 1:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 2:
+  eightBall = 'It is decidedly so';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = "Don't count on it";
+  break;
+  case 5:
+  eightBall = 'My sources say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  case 7:
+  eightBall = 'Signs point to yes';
+  break;
+  
+}
+
+console.log(eightBall);
